7887e2f868985ee6e9857f167ae1f14be9401976
rg <ryan@gedwill.com>
2021-05-20 16:45:08 -0700

    added ssl because of googles new policies

--- email_test.py
+++ email_test.py
@@ -3,7 +3,7 @@
 # send_email('UnitedStates.ReliableLoans', 'unitedstatesreliableloans', 'ryangedwill@gmail.com', 'it works!!!', 'THE BODY WORKS TOO')
 
 def send_email(user, pwd, recipient, subject, body):
-    import smtplib
+    import smtplib, ssl
 
     gmail_user = user
     gmail_pwd = pwd
@@ -11,18 +11,23 @@ def send_email(user, pwd, recipient, subject, body):
     TO = recipient if type(recipient) is list else [recipient]
     SUBJECT = subject
     TEXT = body
+    port = 465  # For SSL
+
+
+
+
 
     # Prepare actual message
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        # TODO: Send email here
         server.ehlo()
-        server.starttls()
         server.login(gmail_user, gmail_pwd)
         server.sendmail(FROM, TO, message)
         server.close()
         print('successfully sent the mail')
-    except:
-        print("failed to send mail")
 
-send_email('UnitedStates.ReliableLoans@gmail.com', 'unitedstatesreliableloans', 'ryangedwill@gmail.com', 'sub', 'bod! </br><a href=\'http://www.google.com\'>click here</a>')
+send_email('ryangedwill@gmail.com', 'GSim1234!', 'ryangedwill@gmail.com', 'sub', 'bod! </br><a href=\'http://www.google.com\'>click here</a>')
